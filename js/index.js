0f98ea509e212d86dc89e500b130d1b7af053355
eddie jimenez <ejmrtears89@gmail.com>
2019-09-09 16:43:25 -0700

    adding 3 event listeners dbl click, click, and key up

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 // Your code goes here
+const funBusQuery = document.querySelector('h1')
+funBusQuery.addEventListener('mouseover', (event) => {
+    funBusQuery.style.color = "green";
+}) 
+
+function changeColor(event) {
+    event.target.style.color = "orange";
+}
+
+const navQuery = document.querySelectorAll('a');
+navQuery.forEach((nav) => {
+    nav.addEventListener('click', changeColor);     
+})
+
+
+const busImg = document.querySelector('.intro img')
+document.addEventListener('keyup', (event) => {
+    if (event.code === 'Space') {
+      busImg.style.display = "none";
+    }
+
+  })
+
+const imgContent = document.querySelector('.img-content img');
+imgContent.addEventListener('dblclick', (event) => {
+    imgContent.setAttribute('src', 'https://i.pinimg.com/originals/cd/96/fe/cd96fef569a44a841a3d7bfe99c697dd.jpg')
+});
+
+
